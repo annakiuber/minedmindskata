14b21a096c077eb3bff97b98af2dc80472d8cdff
anna.kiuber <anna.kiuber@gmail.com>
2018-02-13 09:57:05 -0500

    fixed push

--- minedminds_kata.rb
+++ minedminds_kata.rb
@@ -1,13 +1,13 @@
 counter = 1
 until counter >=101
-	if counter == 3
-		puts "mined"
-	elsif counter % 3 ==0
+	if counter % 15 ==0
 		puts "mined minds"
+	elsif counter % 3 ==0
+		puts "mined"
 	elsif counter == 5
 		puts "minds"
 	elsif counter % 5 ==0
-		puts "mined minds"
+		puts "minds"
 		
 	else 
 		puts counter 
---
+++ playingaround1.rb
@@ -0,0 +1,11 @@
+counter = 1
+	100.times do
+	puts counter
+	counter = counter + 1
+ 		if counter == 5
+    	puts "highfive"
+		elseif counter >= 9 
+		puts "i'm trying to understqand this but it's not sinking in"
+  end
+
+end
