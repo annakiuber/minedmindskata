2a52b1deb687e2e48c23f740c012b507df11d004
anna.kiuber <anna.kiuber@gmail.com>
2018-02-20 16:13:10 -0500

    first and second test after file name change

--- minedminds_kata_function.rb
+++ minedminds_kata_function.rb
@@ -1,7 +1,10 @@
 def minedminds (x)
 	if x == 3
 		return "mined"
+	elsif x ==  5
+		return "minds"
 	else
-		x
+		return x
+		
 	end
 end
--- minedminds_kata_test.rb
+++ minedminds_kata_test.rb
@@ -2,16 +2,12 @@ require "minitest/autorun"
 require_relative "minedminds_kata_function"
 
 class Devision_test < Minitest::Test 
-	def test_practice_equals_1
-		assert_equal(true,true)
-	end
-	def test_1_returns_1
-		assert_equal(1, minedminds(1))
-	end
-	def test_7_returns_7
-		assert_equal(7, minedminds(7))
-	end
 	def test_3_returns_mined
 		assert_equal("mined", minedminds(3))
 	end
+	def test_5_returns_minds
+		assert_equal("minds", minedminds(5))
+	end
 end
+
+
