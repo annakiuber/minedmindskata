b4b9154105a308f6e6cfad6a2c6e5040432f9c4b
anna.kiuber <anna.kiuber@gmail.com>
2018-02-22 11:56:50 -0500

    countertest

--- minedminds_kataarray.rb
+++ minedminds_kataarray.rb
@@ -1,4 +1,10 @@
 def minedminds_array()
 	anna = []
+	counter = 1
+	until counter >= 101
+		anna.push(counter)
+		counter = counter + 1
+	end
+	
 	anna
 end
--- test_minedminds_kata_array.rb
+++ test_minedminds_kata_array.rb
@@ -6,4 +6,9 @@ class Kataarraytest < Minitest::Test
 		m_array = minedminds_array()
 		assert_equal(Array, m_array.class)
 	end
+
+	def test_index_zero_is1
+		m_array = minedminds_array()
+		assert_equal(100, m_array.length)
+	end
 end
