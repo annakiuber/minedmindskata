70b714c6450c966ea1e2825b41c313edf2adca3e
anna.kiuber <anna.kiuber@gmail.com>
2018-02-27 13:27:54 -0500

    i don't get this!!!!!!

--- class_number_string_to_index.rb
+++ class_number_string_to_index.rb
@@ -1,19 +1,25 @@
-def off_by_one(num1, num2)
+def string(num1, num2)
 	matches = 0
 	for x in (0...num1.length)
 		if num1[x] == num2[x]
 		matches += 1
 		end
 	end
-	return matches
+	matches
 	
 end
 
+def off_by_one(my_ticket, winning_array)
 
-def stupid(my_ticket, winning_array)
 	close_array = []
-	close_array.push("1239")
+	winning_array.each do |element|
+		if string(my_ticket, element) == 3
+		close_array.push(element)
+		end
+
+	end
 	close_array
+
 
 end
 
--- test_class_number_string_to_index.rb
+++ test_class_number_string_to_index.rb
@@ -11,31 +11,36 @@ class Addition_test < Minitest::Test
 	def test_integer
 	num1 = "1"
 	num2 = "1"
-	number = off_by_one(num1, num2)
+	number = string(num1, num2)
 	assert_equal(1, number)
 	end
 
 	def test_four_numbers
 		num1 = "1234"
 		num2 = "1234"
-		number = off_by_one(num1, num2)
+		number = string(num1, num2)
 		assert_equal(4, number)
 	end
 
 	def test__three_numbers
 		num1 = "1234"
 		num2 = "1239"
-		number = off_by_one(num1, num2)
+		number = string(num1, num2)
 		assert_equal(3, number)
 	end
 
 	def test_winning_returns_2
 		my_ticket = "1234"
 		winning_array = ["1234", "1239"]
-		number = stupid(my_ticket, winning_array)
+		number = off_by_one(my_ticket, winning_array)
 		assert_equal(["1239"], number)
 	end
 
 
-	
+	def test_one_off_by_one
+		my_ticket = "1234"
+		winning_array = ["5447", "1235", "7998"]
+		number = off_by_one(my_ticket, winning_array)
+		assert_equal(["1235"], number)
+	end
 end
