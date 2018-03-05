9221f84305c9f6fe26087d196697a2591590043f
anna.kiuber <anna.kiuber@gmail.com>
2018-03-05 09:55:04 -0500

    passed with four

--- random_pairing_test.rb
+++ random_pairing_test.rb
@@ -10,4 +10,8 @@ class Pairing_gen_test < Minitest::Test
 	def test_pair_of_two
 		assert_equal(1, annas_pairing_app(["BobbySue", "BillyJoeBob"]).size)
 	end
+
+	def test_pair_of_4
+		assert_equal(2, annas_pairing_app(["Rick", "Morty", "Scott", "Smelly Scott"]).size)
+	end
 end
