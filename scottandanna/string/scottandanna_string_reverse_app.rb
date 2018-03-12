27d70bcc41f6e980aa0066ff6a8616c0f0df9296
anna.kiuber <anna.kiuber@gmail.com>
2018-03-12 11:14:35 -0400

    final reverse test

--- scottandanna/string/scottandanna_string_reverse_app.rb
+++ scottandanna/string/scottandanna_string_reverse_app.rb
@@ -1,4 +1,6 @@
-def reverse_my_string()
-	""
-	
+def reverse_my_string(string)
+	split_my_string = string.split("")#quotes are needed & .split
+	reversed = []
+	string.size.times { reversed << split_my_string.pop} #times is a counter, specified by the .size
+	reversed.join
 end
--- scottandanna/string/scottandanna_string_reverse_test.rb
+++ scottandanna/string/scottandanna_string_reverse_test.rb
@@ -8,6 +8,18 @@ class Reverse_my_string< Minitest::Test
 	end
 
 	def test_for_string
-		assert_equal(String, reverse_my_string.class)
+		assert_equal(String, reverse_my_string("abc").class)
+	end
+
+	def test_reverse
+		assert_equal("olleh", reverse_my_string("hello"))
+	end
+
+	def test_reverse_2
+		assert_equal("anna si eman ym", reverse_my_string("my name is anna"))
+	end
+
+	def test_revese_3
+		assert_equal("margana na s'ti dna anna si eman ym yllacinori", reverse_my_string("ironically my name is anna and it's an anagram"))
 	end
 end
