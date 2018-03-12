2716a8f7cc19931b8d967952d71c5d56aee008e3
anna.kiuber <anna.kiuber@gmail.com>
2018-03-12 12:28:48 -0400

    fixed test

--- scottandanna/string/scottandanna_string_capitolize_test.rb
+++ scottandanna/string/scottandanna_string_capitolize_test.rb
@@ -15,7 +15,7 @@ class Reverse_my_string< Minitest::Test
 		assert_equal("Bring me cookies", no_capitolize_my_string("bring me cookies"))
 	end
 
-	def test_capitolize
+	def test_capitolize2
 		assert_equal("Return my brain please, i lost it", no_capitolize_my_string("return my brain please, i lost it"))
 	end
 end
