7afc769c2a5f9c864ef46751ebeea9166eac5837
anna.kiuber <anna.kiuber@gmail.com>
2018-03-13 14:58:37 -0400

    passing tests for class hash excersize

--- codeword/codewordapp.rb
+++ codeword/codewordapp.rb
@@ -1,3 +1,14 @@
 def codeword(error)
-	"page not found."
+	message = ""
+	error_hash = { 404 => "page not found.", 1942 => "grandpa was found", 402 => "page almost found"}
+	error_hash.each_key { |key|
+		if key == error 
+			message = error_hash[key]
+		end
+		
+	}
+		message
+
+	
+	
 end
