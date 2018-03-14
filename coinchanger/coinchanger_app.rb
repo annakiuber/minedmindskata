090e7eaca5017d47810a929837053f71f698e79d
anna.kiuber <anna.kiuber@gmail.com>
2018-03-14 12:26:07 -0400

    repoe for changeapp

--- coinchanger/coinchanger_app.rb
+++ coinchanger/coinchanger_app.rb
@@ -24,8 +24,13 @@ def plural_hash(coins)
 	coin_plural = {}
 	coins.each{|key,value|
 		if value > 1
+			if key == "quarter"
 			coin_plural["quarters"] = value
+			elsif key == "dime"
+				coin_plural["dimes"] = value
+				
+			end
 		end
 	}
 	return coin_plural
-	end
+end
